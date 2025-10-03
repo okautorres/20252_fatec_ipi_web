59028bc62eb58ba40f0333752068e7c2a76fa0b0
Kauan Torres <darqueboost@gmail.com>
2025-10-02 21:52:37 -0300

    feat: frontend sendo construido

---
+++ frontend/lojaquadros/src/app/app.routes.ts
@@ -0,0 +1,18 @@
+
+import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Sobre } from './sobre/sobre';
+import { Usuarios } from './usuarios/usuarios';
+
+export const routes: Routes = [
+
+  { path: 'home', component: Home },
+
+
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  
+
+  { path: 'sobre', component: Sobre },
+  { path: 'usuarios', component: Usuarios },
+
+];
